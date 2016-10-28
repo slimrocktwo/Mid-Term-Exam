bd82cd561efa00941b243b69ed91e418c66be6b6
slimrocktwo <slimrocktwo2@hotmail.com>
2016-10-27 22:05:39 -0400

    Create CreateExam.sh

---
+++ CreateExam.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##This file was used to originaly create the MidTerm directory and the EXAM.sh file using VI,
+##As well as setting privlages.
+
+mkdir /home/slangewicz/MidTerm/
+cd /home/slangewicz/MidTerm/
+touch EXAM.sh
+chmod 704 EXAM.sh
